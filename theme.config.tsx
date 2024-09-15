b2510756b6da9fb64abc61e53a940e8d572de5c4
DabAZ <dabaz@dabaz.me>
2024-09-15 21:08:48 +0800

    add basic layout

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DabPython</span>,
   project: {
     link: 'https://github.com/dabaz_official',
   },
